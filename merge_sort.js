238c02c7dfc86490b1c0b44b3a6880ebfe683056
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-08-28 07:12:52 +0100

    Create the mergeSort function

---
+++ merge_sort.js
@@ -0,0 +1,32 @@
+// MERGE SORT PART
+
+// Create a helper function that will compare and merge
+
+function merge(left, right) {
+  if (left.length === 0) return right;
+  if (right.length === 0) return left;
+
+  if (left[0] < right[0]) {
+    return [left[0]].concat(merge(left.slice(1), right));
+  } else {
+    return [right[0]].concat(merge(left, right.slice(1)));
+  }
+}
+
+
+function mergeSort(array){ 
+ if (array.length <= 1) {
+   return array; 
+}
+   let middle = Math.floor(array.length/2)
+   let leftArray = array.slice(0,middle)
+   let rightArray = array.slice(middle)
+
+   const sortedLeftArray = mergeSort(leftArray); 
+   const sortedRightArray = mergeSort(rightArray); 
+   
+   return merge(sortedLeftArray, sortedRightArray);  
+}
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])); 
+console.log(mergeSort([105, 79, 100, 110])); 
